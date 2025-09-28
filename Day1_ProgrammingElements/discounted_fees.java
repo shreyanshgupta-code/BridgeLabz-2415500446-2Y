a626666e20c5a5a062930461610f912629091a68
Shreyansh Gupta <shreyanshgupta131977@gmail.com>
2025-09-28 19:33:14 +0530

    first commit

--- Day1_ProgrammingElements/discounted_fees.java
+++
@@ -1,9 +0,0 @@
-public class discounted_fees {
-    public static void main(String[] args) {
-        int fees=125000;
-        int discount=10;
-        int discountfees=(discount*fees)/100;
-        int feestopay=fees-discountfees;
-        System.out.println("The discount amount is INR " + discountfees+" and final discounted fee is INR "+ feestopay);
-    }
-}
--- Day1_ProgrammingElements/kilometer_to_miles.java
+++
@@ -1,7 +0,0 @@
-public class kilometer_to_miles {
-    public static void main(String[] args) {
-        double kilometer=10.8;
-        double miles=kilometer/1.6;
-        System.out.print("The distance 10.8 KM in miles is: "+ miles+" miles");
-    }
-}
