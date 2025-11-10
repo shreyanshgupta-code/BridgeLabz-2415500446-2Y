09d0073cc837e37d1e970035f79a116a08367a83
Shreyansh Gupta <shreyanshgupta131977@gmail.com>
2025-11-10 23:20:44 +0530

    first commit

--- Exception_Handling/bank_transaction_system/InsufficientBalanceException.java
+++
@@ -1,7 +0,0 @@
-package bank_transaction_system;
-
-public class InsufficientBalanceException extends Exception {
-    public InsufficientBalanceException(String message) {
-        super(message);
-    }
-}
--- Exception_Handling/custom_exception/InvalidAgeException.java
+++
@@ -1,7 +0,0 @@
-package custom_exception;
-
-public class InvalidAgeException extends Exception {
-    public InvalidAgeException(String message) {
-        super(message);
-    }
-}
